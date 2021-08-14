139d8ba37d0ff6931949bf5af66e8634c1dc5a17
alsignat <alsignat@gmail.com>
2021-08-14 23:53:36 +0300

    5. Конвертер валюты [#41583]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -7,15 +7,38 @@ public class Converter {
         return rsl;
     }
 
+    public static float euroToRuble(float value) {
+        float rsl = value * 70;
+        return rsl;
+    }
+
     public static float rubleToDollar(float value) {
-        float rsl = value / 60; /* формула перевода рублей в доллары. */
+        float rsl = value / 60;
+        return rsl;
+    }
+
+    public static float dollarToRuble(float value) {
+        float rsl = value * 60;
+        return rsl;
+    }
+
+    public static float dollarToEuro(float value) {
+        float rsl = value * 60 / 70;
+        return rsl;
+    }
+
+    public static float euroToDollar(float value) {
+        float rsl = value * 70 / 60;
         return rsl;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        float dollars = Converter.rubleToDollar(240);
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("240 rubles are " + dollars + " dollars.");
+        float euro = Converter.rubleToEuro(140) + Converter.dollarToEuro(15);
+        float dollars = Converter.rubleToDollar(240) + Converter.euroToDollar(30);
+        float rubles = Converter.dollarToRuble(10) + Converter.euroToRuble(20);
+
+        System.out.println("140 rubles plus 10 dollars are " + String.format("%.2f", euro) + " euro.");
+        System.out.println("240 rubles plus 30 euro are " + String.format("%.2f", dollars) + " dollars.");
+        System.out.println("10 dollars plus 20 euro are " + String.format("%.2f", rubles) + " rubles.");
     }
 }

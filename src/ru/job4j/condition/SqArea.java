8a868e4ccf3aee81249e8b4e58fb772cb7a93ab2
alsignat <alsignat@gmail.com>
2021-08-16 19:50:19 +0300

    Исправлено

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,8 +2,9 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(double p, double k) {
-        double rsl = k * Math.pow(p / (2 * (k + 1)), 2);
-        return rsl;
+        double height = p / (2 * (k + 1));
+        double length = height * k;
+        return length * height;
     }
 
     public static void main(String[] args) {

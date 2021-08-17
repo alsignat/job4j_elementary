c7d9fff35ec8d13ab4f6dcc68a5e40aceaf76a1c
alsignat <alsignat@gmail.com>
2021-08-17 21:13:26 +0300

    1.2. Входные данные в тесте [#297750 #190674]

--- test/ru/job4j/condition/X2Test.java
+++ test/ru/job4j/condition/X2Test.java
@@ -8,81 +8,56 @@ public class X2Test {
 
     @Test
     public void testingA() {
-        // Входные параметра
         int a = 10;
         int b = 0;
         int c = 0;
         int x = 2;
-        // ожидаемое значение
         int expected = 40;
-       // вызов метода, который хотим проверить.
-        // Результат вычисления. Это всегда одна переменная.
         int rsl = X2.calc(a, b, c, x);
-       // Сравнение
         assertEquals(expected, rsl);
     }
 
     @Test
     public void testingB() {
-        // Входные параметра
         int a = 1;
         int b = 1;
         int c = 1;
         int x = 1;
-        // ожидаемое значение
         int expected = 3;
-        // вызов метода, который хотим проверить.
-        // Результат вычисления. Это всегда одна переменная.
         int rsl = X2.calc(a, b, c, x);
-        // Сравнение
         assertEquals(expected, rsl);
     }
 
     @Test
     public void testingC() {
-        // Входные параметра
         int a = 0;
         int b = 1;
         int c = 1;
         int x = 1;
-        // ожидаемое значение
         int expected = 2;
-        // вызов метода, который хотим проверить.
-        // Результат вычисления. Это всегда одна переменная.
         int rsl = X2.calc(a, b, c, x);
-        // Сравнение
         assertEquals(expected, rsl);
     }
 
     @Test
     public void testingD() {
-        // Входные параметра
         int a = 1;
         int b = 1;
         int c = 0;
         int x = 1;
-        // ожидаемое значение
         int expected = 2;
-        // вызов метода, который хотим проверить.
-        // Результат вычисления. Это всегда одна переменная.
         int rsl = X2.calc(a, b, c, x);
-        // Сравнение
         assertEquals(expected, rsl);
     }
 
     @Test
     public void testingE() {
-        // Входные параметра
         int a = 1;
         int b = 1;
         int c = 1;
         int x = 0;
-        // ожидаемое значение
         int expected = 1;
-        // вызов метода, который хотим проверить.
-        // Результат вычисления. Это всегда одна переменная.
         int rsl = X2.calc(a, b, c, x);
-        // Сравнение
         assertEquals(expected, rsl);
     }
 }

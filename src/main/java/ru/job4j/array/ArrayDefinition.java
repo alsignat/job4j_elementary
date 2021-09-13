0d1be0664b504ebb54a2be268616950809121943
alsignat <alsignat@gmail.com>
2021-09-13 15:50:35 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println(surnames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Hello Worldov";
+        names[1] = "Foo Barov";
+        names[2] = "World Hellovyan";
+        names[3] = "Bar Foofuev";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }

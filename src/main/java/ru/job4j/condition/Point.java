6b2d1d19d07b2ba09d42ce3e474a4304d9b2dfa1
alsignat <alsignat@gmail.com>
2021-09-13 12:00:35 +0300

    style corrections according to checkstyle

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -11,12 +11,12 @@ public class Point {
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
-        System.out.println("result (0, 0) to (2, 2) " + Point.distance(0,0,2,2));
-        System.out.println("result (0, 0) to (2, 0) " + Point.distance(0,1,2,2));
-        System.out.println("result (0, 0) to (2, 0) " + Point.distance(-5,1,3,10));
-        System.out.println("result (0, 0) to (2, 0) " + Point.distance(1,-5,0,8));
-        System.out.println("result (0, 0) to (2, 0) " + Point.distance(0,0,0,0));
-        System.out.println("result (0, 0) to (2, 0) " + Point.distance(1,1,-1,-5));
-        System.out.println("result (0, 0) to (2, 0) " + Point.distance(13,200,-1,10));
+        System.out.println("result (0, 0) to (2, 2) " + Point.distance(0, 0, 2, 2));
+        System.out.println("result (0, 0) to (2, 0) " + Point.distance(0, 1, 2, 2));
+        System.out.println("result (0, 0) to (2, 0) " + Point.distance(-5, 1, 3, 10));
+        System.out.println("result (0, 0) to (2, 0) " + Point.distance(1, -5, 0, 8));
+        System.out.println("result (0, 0) to (2, 0) " + Point.distance(0, 0, 0, 0));
+        System.out.println("result (0, 0) to (2, 0) " + Point.distance(1, 1, -1, -5));
+        System.out.println("result (0, 0) to (2, 0) " + Point.distance(13, 200, -1, 10));
     }
 }

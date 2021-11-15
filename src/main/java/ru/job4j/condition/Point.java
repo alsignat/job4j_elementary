0b85ea79f06bef1b0d204cba890b5bf776ebe02d
alsignat <alsignat@gmail.com>
2021-11-15 21:15:15 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,22 +1,27 @@
 package ru.job4j.condition;
 
-import java.util.concurrent.locks.Condition;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return rsl;
+
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point point) {
+
+        return sqrt(pow(this.x - point.x, 2) + pow(this.y - point.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        System.out.println("result (0, 0) to (2, 2) " + Point.distance(0, 0, 2, 2));
-        System.out.println("result (0, 0) to (2, 0) " + Point.distance(0, 1, 2, 2));
-        System.out.println("result (0, 0) to (2, 0) " + Point.distance(-5, 1, 3, 10));
-        System.out.println("result (0, 0) to (2, 0) " + Point.distance(1, -5, 0, 8));
-        System.out.println("result (0, 0) to (2, 0) " + Point.distance(0, 0, 0, 0));
-        System.out.println("result (0, 0) to (2, 0) " + Point.distance(1, 1, -1, -5));
-        System.out.println("result (0, 0) to (2, 0) " + Point.distance(13, 200, -1, 10));
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
